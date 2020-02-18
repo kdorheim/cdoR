dc81138035563254ff3e3afbcd38cd5682c57572
Kalyn <kalyn.dorheim@pnnl.gov>
2020-02-18 12:57:10 -0500

    Add a test to check to make sure that the fld mean calcualtes the weighted area.

--- tests/testthat/test_cdo_fxns.R
+++ tests/testthat/test_cdo_fxns.R
@@ -1,69 +1,86 @@
 context('Test cdo functions.')
 
-# Define the directory to write out all of the intermediate netcdf files to.
-test_dir <- getwd()
+# Only test the cdo functions if the correct environment is set up for it.
+if(dir.exists(cdoR::cdo_exe)){
 
-# Create a fake cmip data frame for the test data.
-dt <- tibble::tibble(file = c(file.path(test_dir, "data/areacella_fx_CNRM-CM6-1_historical_r10i1p1f2_gr.nc"),
-                                      file.path(test_dir, "data/sftlf_fx_CNRM-CM6-1_historical_r10i1p1f2_gr.nc"),
-                                      file.path(test_dir, "data/tas_Amon_CNRM-CM6-1_historical_r10i1p1f2_gr_185001-201412.nc")),
-                             variable = c('areacella', 'sftlf', 'tas'),
-                             domain = c('domain', 'domain', 'domain'),
-                             model = c('model', 'model', 'model'),
-                             experiment = c('experiment', 'experiment', 'experiment'),
-                             ensemble = c('ensemble', 'ensemble', 'ensemble'),
-                             grid = c('grid', 'grid', 'grid'))
+  # Define the directory to write out all of the intermediate netcdf files to.
+  test_dir <- getwd()
 
-
-# Format the input data for the cdo land and ocean area weights.
-#area_in <- dcast(dt, domain + model + experiment + ensemble + grid ~ variable, value.var = 'file')
-area_in <- tidyr::spread(dt, variable, file)
-tas_nc <- dt$file[dt$variable == 'tas']
-dt_tas <- dt[dt$variable == 'tas']
-
-land_area <- cdo_land_area(dt = area_in, intermed_dir = test_dir)
-ocean_area <- cdo_ocean_area(area_in, test_dir)
-
-testthat::test_that('test environement set up correctly', {
-
-testthat::expect_true(all(file.exists(dt$file)))
-
-})
-
-testthat::test_that('generate area weights works', {
-
-  # Extract the total area, ocean, and land areas should equal the total area.
-  # Since the oceans cover 70% of the globe the ratio of the ocean to land area
-  # should reflect this.
-  land_area_rslt  <- sum(ncdf4::ncvar_get(ncdf4::nc_open(land_area), 'areacella'))
-  ocean_area_rslt <- sum(ncdf4::ncvar_get(ncdf4::nc_open(ocean_area), 'areacella'))
-  globe_area_rslt <- sum(ncdf4::ncvar_get(ncdf4::nc_open(area_in[['areacella']]), 'areacella'))
-
-  testthat::expect_equal(land_area_rslt + ocean_area_rslt, globe_area_rslt, tolerance = 0.1)
-  testthat::expect_equal(ocean_area_rslt / land_area_rslt, 70 / 30,  tolerance = 0.1)
-
-})
-
-cat_tas <- cdo_concate(dt = dt_tas, intermed_dir = test_dir)
-
-testthat::test_that('cdo_concate runs',{
-  testthat::expect_true(file.exists(cat_tas))
-})
+  # Create a fake cmip data frame for the test data.
+  dt <- tibble::tibble(file = c(file.path(test_dir, "data/areacella_fx_CNRM-CM6-1_historical_r10i1p1f2_gr.nc"),
+                                file.path(test_dir, "data/sftlf_fx_CNRM-CM6-1_historical_r10i1p1f2_gr.nc"),
+                                file.path(test_dir, "data/tas_Amon_CNRM-CM6-1_historical_r10i1p1f2_gr_185001-201412.nc")),
+                       variable = c('areacella', 'sftlf', 'tas'),
+                       domain = c('domain', 'domain', 'domain'),
+                       model = c('model', 'model', 'model'),
+                       experiment = c('experiment', 'experiment', 'experiment'),
+                       ensemble = c('ensemble', 'ensemble', 'ensemble'),
+                       grid = c('grid', 'grid', 'grid'))
 
 
-testthat::test_that('fldmean works', {
+  # Format the input data for the cdo land and ocean area weights.
+  #area_in <- dcast(dt, domain + model + experiment + ensemble + grid ~ variable, value.var = 'file')
+  area_in <- tidyr::spread(dt, variable, file)
+  tas_nc <- dt[dt$variable == 'tas', ][['file']]
+  dt_tas <- dt[dt$variable == 'tas', ]
+  info   <- parse_cmip_info(dt_tas)
 
-  # Calculate the weighted means with the different area weights, then compare temperature.
-  ## TODO we are currently using an R based function which is going to be slow, but we should
-  ## be able to
-  land_mean   <- cdo_fldmean_area(name = 'land', in_nc = tas_nc, area_nc = land_area, intermed_dir = test_dir)
-  ocean_mean  <- cdo_fldmean_area(name = 'ocean', in_nc = tas_nc, area_nc = ocean_area, intermed_dir = test_dir)
-  global_mean <- cdo_fldmean_area(name = 'globe', in_nc = tas_nc, area_nc = area_in[['areacella']], intermed_dir = test_dir)
+  land_area <- cdo_land_area(dt = area_in, intermed_dir = test_dir)
+  ocean_area <- cdo_ocean_area(area_in, test_dir)
 
-  land_tas    <- ncdf4::ncvar_get(ncdf4::nc_open(land_mean), 'tas')
-  ocean_tas   <- ncdf4::ncvar_get(ncdf4::nc_open(ocean_mean), 'tas')
-  glaobal_tas <- ncdf4::ncvar_get(ncdf4::nc_open(global_mean), 'tas')
+  testthat::test_that('test environement set up correctly', {
 
-})
+    testthat::expect_true(all(file.exists(dt$file)))
 
-file.remove(list.files(test_dir, pattern = '.nc'))
+  })
+
+  testthat::test_that('generate area weights works', {
+
+    # Extract the total area, ocean, and land areas should equal the total area.
+    # Since the oceans cover 70% of the globe the ratio of the ocean to land area
+    # should reflect this.
+    land_area_rslt  <- sum(ncdf4::ncvar_get(ncdf4::nc_open(land_area), 'areacella'))
+    ocean_area_rslt <- sum(ncdf4::ncvar_get(ncdf4::nc_open(ocean_area), 'areacella'))
+    globe_area_rslt <- sum(ncdf4::ncvar_get(ncdf4::nc_open(area_in[['areacella']]), 'areacella'))
+
+    testthat::expect_equal(land_area_rslt + ocean_area_rslt, globe_area_rslt, tolerance = 0.1)
+    testthat::expect_equal(ocean_area_rslt / land_area_rslt, 70 / 30,  tolerance = 0.1)
+
+  })
+
+  cat_tas <- cdo_concate(dt = dt_tas, intermed_dir = test_dir)
+
+  testthat::test_that('cdo_concate runs',{
+    testthat::expect_true(file.exists(cat_tas))
+  })
+
+
+  testthat::test_that('fldmean works', {
+
+    # Calculate the weighted means with the different area weights, then compare temperature.
+    ## TODO we are currently using an R based function which is going to be slow, but we should
+    ## be able to make it work.
+    land_mean   <- fldmean_area(info = info, in_nc = tas_nc, area_nc = land_area)
+    ocean_mean  <- fldmean_area(info = info, in_nc = tas_nc, area_nc = ocean_area)
+    global_mean <- fldmean_area(info = info, in_nc = tas_nc, area_nc = area_in[['areacella']])
+
+    # The ocean, land, and global mean temperature should be different from one another.
+    testthat::expect_true(mean((land_mean$value - ocean_mean$value)^2) > 0)
+    testthat::expect_true(mean((land_mean$value - global_mean$value)^2) > 0)
+
+  })
+
+  file.remove(list.files(test_dir, pattern = '.nc'))
+
+
+} else {
+
+  testthat::test_that('Skipping CDO Related Functions', {
+
+
+  })
+
+
+}
+
+
